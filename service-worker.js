4da5616b5019a91adf60818eddc9fce6fb084efc
Aishat Muh'd Ize <izeaishat@gmail.com>
2026-02-25 10:07:21 +0100

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,22 @@
-self.addEventListener("install", event => {
+const cacheName = 'nysc-smart-cache-v1';
+const assetsToCache = [
+  '/',
+  '/nysc-smart-companion/index.html',
+  '/nysc-smart-companion/corper.png',
+  '/nysc-smart-companion/icon-192.png',
+  '/nysc-smart-companion/icon-512.png',
+  '/nysc-smart-companion/manifest.json'
+];
+
+self.addEventListener('install', e=>{
+  e.waitUntil(caches.open(cacheName).then(cache=>cache.addAll(assetsToCache)));
   self.skipWaiting();
 });
 
-self.addEventListener("activate", event => {
-  event.waitUntil(clients.claim());
+self.addEventListener('activate', e=>{
+  e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==cacheName).map(k=>caches.delete(k)))));
+});
+
+self.addEventListener('fetch', e=>{
+  e.respondWith(caches.match(e.request).then(resp=>resp || fetch(e.request)));
 });
