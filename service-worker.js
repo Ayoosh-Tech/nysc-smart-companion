6b863c5a9bd07e3a512ccdb335c92943c6616faa
Aishat Muh'd Ize <izeaishat@gmail.com>
2026-02-25 14:03:45 +0100

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,7 @@
 const cacheName = 'nysc-smart-cache-v1';
+
 const assetsToCache = [
-  '/',
+  '/nysc-smart-companion/',
   '/nysc-smart-companion/index.html',
   '/nysc-smart-companion/corper.png',
   '/nysc-smart-companion/icon-192.png',
@@ -8,15 +9,26 @@ const assetsToCache = [
   '/nysc-smart-companion/manifest.json'
 ];
 
-self.addEventListener('install', e=>{
-  e.waitUntil(caches.open(cacheName).then(cache=>cache.addAll(assetsToCache)));
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(cacheName).then(cache => cache.addAll(assetsToCache))
+  );
   self.skipWaiting();
 });
 
-self.addEventListener('activate', e=>{
-  e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==cacheName).map(k=>caches.delete(k)))));
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.filter(key => key !== cacheName)
+            .map(key => caches.delete(key))
+      )
+    )
+  );
 });
 
-self.addEventListener('fetch', e=>{
-  e.respondWith(caches.match(e.request).then(resp=>resp || fetch(e.request)));
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(resp => resp || fetch(e.request))
+  );
 });
